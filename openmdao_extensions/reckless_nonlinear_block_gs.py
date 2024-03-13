5cb0f05c040108cdaab8de6f61ceea86c4616893
relf <remi.lafage@onera.fr>
2024-03-13 17:44:17 +0100

    Merge with openmdao 3.30+ Solver _solve (#1930c18)

--- openmdao_extensions/reckless_nonlinear_block_gs.py
+++ openmdao_extensions/reckless_nonlinear_block_gs.py
@@ -6,9 +6,9 @@ This class was derived from OpenMDAO 3.30 NonlinearBlockGS code
 import os
 import numpy as np
 
-from openmdao.core.analysis_error import AnalysisError
 from openmdao.recorders.recording_iteration_stack import Recording
 from openmdao.api import NonlinearBlockGS
+from openmdao.utils.om_warnings import issue_warning, SolverWarning
 
 
 class RecklessNonlinearBlockGS(NonlinearBlockGS):
@@ -69,104 +69,150 @@ class RecklessNonlinearBlockGS(NonlinearBlockGS):
         """
         Run the iterative solver.
 
-        Overrides opendmao/solvers/solver.py to implement _is_rtol_converged
+        Overrides solve() from opendmao/solvers/solver.py to implement _is_rtol_converged
         """
-        maxiter = self.options["maxiter"]
-        atol = self.options["atol"]
-        iprint = self.options["iprint"]
+        system = self._system()
 
-        self._mpi_print_header()
+        print(system._relevant)
 
-        self._iter_count = 0
-        self._iter_initialize()
+        with system._relevant.active(self.use_relevance()):
+            maxiter = self.options["maxiter"]
+            atol = self.options["atol"]
+            rtol = self.options["rtol"]
+            iprint = self.options["iprint"]
+            stall_limit = self.options["stall_limit"]
+            stall_tol = self.options["stall_tol"]
+            stall_tol_type = self.options["stall_tol_type"]
 
-        if self._convrg_vars:
-            # get initial value of convrg_vars
-            norm, val_convrg_vars_minus1 = self._iter_get_norm()
-            ratio = 1
-        else:
-            norm = self._iter_get_norm()
-            norm0 = norm if norm != 0.0 else 1.0
-            ratio = norm / norm0
+            self._mpi_print_header()
 
-        self._mpi_print(self._iter_count, norm, ratio)
-        is_rtol_converged = self._is_rtol_converged(ratio)
+            self._iter_count = 0
+            self._iter_initialize()
 
-        while self._iter_count < maxiter and norm > atol and not is_rtol_converged:
-            with Recording(type(self).__name__, self._iter_count, self) as rec:
-                self._single_iteration()
-                self._iter_count += 1
-                self._run_apply()
-
-                if self._convrg_vars:
-                    norm, val_convrg_vars = self._iter_get_norm()
-                    rec.abs = norm
-                    rec.rel = (
-                        np.abs(val_convrg_vars - val_convrg_vars_minus1)
-                        / val_convrg_vars_minus1
-                    )
-                    ratio = (
-                        np.abs(val_convrg_vars - val_convrg_vars_minus1)
-                        / val_convrg_vars_minus1
-                    )
-                    ratio = np.max(ratio)
-
-                    val_convrg_vars_minus1 = val_convrg_vars
-                else:
-                    norm = self._iter_get_norm()
-                    # With solvers, we want to record the norm AFTER the call, but the call needs to
-                    # be wrapped in the with for stack purposes, so we locally assign  norm & norm0
-                    # into the class.
-                    rec.abs = norm
-                    rec.rel = norm / norm0
-                    ratio = norm / norm0
-                    if norm0 == 0:
-                        norm0 = 1
+            if self._convrg_vars:
+                # get initial value of convrg_vars
+                norm, val_convrg_vars_minus1 = self._iter_get_norm()
+                ratio = 1
+            else:
+                norm = self._iter_get_norm()
+                norm0 = norm if norm != 0.0 else 1.0
+                ratio = norm / norm0
 
             self._mpi_print(self._iter_count, norm, ratio)
-            is_rtol_converged = self._is_rtol_converged(ratio)
+            is_rtol_converged = self._is_rtol_converged(rtol, ratio)
 
-        system = self._system()
-        if system.comm.rank == 0 or os.environ.get("USE_PROC_FILES"):
-            prefix = self._solver_info.prefix + self.SOLVER
-            is_rtol_converged = self._is_rtol_converged(ratio)
-            # Solver terminated early because a Nan in the norm doesn't satisfy the while-loop
-            # conditionals.
-            if np.isinf(norm) or np.isnan(norm):
-                msg = (
-                    "Solver '{}' on system '{}': residuals contain 'inf' or 'NaN' after {} "
-                    + "iterations."
-                )
-                if iprint > -1:
-                    print(
-                        prefix
-                        + msg.format(self.SOLVER, system.pathname, self._iter_count)
-                    )
+            stalled = False
+            stall_count = 0
+            if stall_limit > 0:
+                stall_norm = norm0
 
-                # Raise AnalysisError if requested.
-                if self.options["err_on_non_converge"]:
-                    raise AnalysisError(
-                        msg.format(self.SOLVER, system.pathname, self._iter_count)
-                    )
+            force_one_iteration = system.under_complex_step
 
-            # Solver hit maxiter without meeting desired tolerances.
-            elif norm > atol and not is_rtol_converged:
-                msg = "Solver '{}' on system '{}' failed to converge in {} iterations."
+            while (
+                self._iter_count < maxiter
+                and norm > atol
+                and not is_rtol_converged
+                and not stalled
+            ) or force_one_iteration:
+                if system.under_complex_step:
+                    force_one_iteration = False
 
-                if iprint > -1:
-                    print(
-                        prefix
-                        + msg.format(self.SOLVER, system.pathname, self._iter_count)
-                    )
+                with Recording(type(self).__name__, self._iter_count, self) as rec:
+                    ls = self.linesearch
+                    if (
+                        stall_count == 3
+                        and ls
+                        and not ls.options["print_bound_enforce"]
+                    ):
+                        self.linesearch.options["print_bound_enforce"] = True
 
-                # Raise AnalysisError if requested.
-                if self.options["err_on_non_converge"]:
-                    raise AnalysisError(
-                        msg.format(self.SOLVER, system.pathname, self._iter_count)
-                    )
+                        if self._system().pathname:
+                            pathname = f"{self._system().pathname}."
+                        else:
+                            pathname = ""
 
-            # Solver converged
-            elif iprint == 1:
+                        msg = (
+                            "Your model has stalled three times and may be violating the bounds."
+                            " In the future, turn on print_bound_enforce in your solver options "
+                            f"here: \n{pathname}nonlinear_solver.linesearch.options"
+                            "['print_bound_enforce']=True. \nThe bound(s) being violated now "
+                            "are:\n"
+                        )
+                        issue_warning(msg, category=SolverWarning)
+
+                        self._single_iteration()
+                        self.linesearch.options["print_bound_enforce"] = False
+                    else:
+                        self._single_iteration()
+
+                    self._iter_count += 1
+                    self._run_apply()
+
+                    if self._convrg_vars:
+                        norm, val_convrg_vars = self._iter_get_norm()
+                        rec.abs = norm
+                        rec.rel = (
+                            np.abs(val_convrg_vars - val_convrg_vars_minus1)
+                            / val_convrg_vars_minus1
+                        )
+                        ratio = (
+                            np.abs(val_convrg_vars - val_convrg_vars_minus1)
+                            / val_convrg_vars_minus1
+                        )
+                        ratio = np.max(ratio)
+
+                        val_convrg_vars_minus1 = val_convrg_vars
+                    else:
+                        norm = self._iter_get_norm()
+                        # With solvers, we want to record the norm AFTER the call, but the call needs to
+                        # be wrapped in the with for stack purposes, so we locally assign  norm & norm0
+                        # into the class.
+                        rec.abs = norm
+                        rec.rel = norm / norm0
+                        ratio = norm / norm0
+                        if norm0 == 0:
+                            norm0 = 1
+
+                    # Check if convergence is stalled.
+                    if stall_limit > 0:
+                        norm_for_stall = rec.rel if stall_tol_type == "rel" else rec.abs
+                        norm_diff = np.abs(stall_norm - norm_for_stall)
+                        if norm_diff <= stall_tol:
+                            stall_count += 1
+                            if stall_count >= stall_limit:
+                                stalled = True
+                        else:
+                            stall_count = 0
+                            stall_norm = norm_for_stall
+
+                self._mpi_print(self._iter_count, norm, ratio)
+                is_rtol_converged = self._is_rtol_converged(rtol, ratio)
+
+        # flag for the print statements. we only print on root if USE_PROC_FILES is not set to True
+        print_flag = system.comm.rank == 0 or os.environ.get("USE_PROC_FILES")
+
+        prefix = self._solver_info.prefix + self.SOLVER
+
+        # Solver terminated early because a Nan in the norm doesn't satisfy the while-loop
+        # conditionals.
+        if np.isinf(norm) or np.isnan(norm):
+            self._inf_nan_failure()
+
+        # solver stalled.
+        elif stalled:
+            msg = (
+                f"Solver '{self.SOLVER}' on system '{system.pathname}' stalled after "
+                f"{self._iter_count} iterations."
+            )
+            self.report_failure(msg)
+
+        # Solver hit maxiter without meeting desired tolerances.
+        elif norm > atol and not is_rtol_converged:
+            self._convergence_failure()
+
+        # Solver converged
+        elif print_flag:
+            if iprint == 1:
                 print(prefix + " Converged in {} iterations".format(self._iter_count))
             elif iprint == 2:
                 print(prefix + " Converged")
@@ -197,16 +243,16 @@ class RecklessNonlinearBlockGS(NonlinearBlockGS):
 
         return super()._iter_initialize()
 
-    def _is_rtol_converged(self, ratio):
+    def _is_rtol_converged(self, rtol, ratio):
         """
         Check convergence regarding relative error tolerance.
 
         Parameters
         ----------
+        rtol : float
+            relative error tolerance
         norm : float
             error (residuals norm)
-        norm0 : float
-            initial error
 
         Returns
         -------
@@ -222,10 +268,9 @@ class RecklessNonlinearBlockGS(NonlinearBlockGS):
                 outputs = system._outputs._views[name]
                 residual = system._residuals._views[name]
                 rerrs[i] = np.linalg.norm(residual) / np.linalg.norm(outputs)
-            is_rtol_converged = (rerrs < self._convrg_rtols).all()
-            is_rtol_converged = ratio < self.options["rtol"]
+            is_rtol_converged = (rerrs < self._convrg_rtols).all() and ratio < rtol
         else:
-            is_rtol_converged = ratio < self.options["rtol"]
+            is_rtol_converged = ratio < rtol
         return is_rtol_converged
 
     def _iter_get_norm(self):
